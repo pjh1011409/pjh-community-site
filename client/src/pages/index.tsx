c47f96e0d45ebe69d4614bed7c4b73d6f95d89b4
ParkJeongHo <pjh31208@naver.com>
2022-11-22 00:43:49 +0900

    Client initial commit
    1. Create next.js app with typescript
    2. Install tailwind css into the app's environment

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,7 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return <div className="text-red-900 text-6xl font-black">안녕</div>;
+}
