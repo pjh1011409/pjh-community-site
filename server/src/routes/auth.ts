2847869d7d5a1e589b1f708b3a97d962d79041aa
ParkJeongHo <pjh31208@naver.com>
2022-11-23 02:09:04 +0900

    feat : Create an API for login request
    토큰, 쿠키 처리 추가

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,9 @@
-import { validate } from 'class-validator';
+import { isEmpty, validate } from 'class-validator';
 import { Request, Response, Router } from 'express';
 import User from '../entities/User';
+import jwt from 'jsonwebtoken';
+import cookie from 'cookie';
+import bcrypt from 'bcryptjs';
 
 const mapErrors = (errors: any[]) => {
   return errors.reduce((prev: any, err: any) => {
@@ -43,7 +46,49 @@ const register = async (req: Request, res: Response) => {
   }
 };
 
+const login = async (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  try {
+    const errors: any = {};
+    if (isEmpty(username)) errors.username = '사용자 이름은 작성해주세요';
+    if (isEmpty(password)) errors.password = '비밀번호를 작성해주세요';
+    if (Object.keys(errors).length > 0) {
+      return res.status(400).json(errors);
+    }
+
+    const user = await User.findOneBy({ username });
+
+    if (!user)
+      return res
+        .status(404)
+        .json({ username: '사용자 이름이 등록되지 않았습니다.' });
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return res.status(401).json({ password: '비밀번호가 잘못되었습니다.' });
+    }
+
+    const token = jwt.sign({ username }, process.env.JWT_SECRET);
+
+    res.set(
+      'Set-Cookie',
+      cookie.serialize('token', token, {
+        httpOnly: true,
+        maxAge: 60 * 60 * 24 * 7,
+        path: '/',
+      })
+    );
+
+    return res.json({ user, token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
 const router = Router();
 router.post('/register', register);
+router.post('/login', login);
 
 export default router;
