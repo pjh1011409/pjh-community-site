578b3583e01a46d0c46003e9a02f0052e16247db
ParkJeongHo <pjh31208@naver.com>
2022-11-22 17:59:10 +0900

    feat : Create registration function
    회원가입에 대한 기능 추가

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+import { useState } from 'react';
+import InputGroup from '../components/InputGroup';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Axios from 'axios';
+
+const register = () => {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState<any>({});
+
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const res = await Axios.post('/auth/register', {
+        email,
+        password,
+        username,
+      });
+      console.log('res:', res);
+      router.push('/login');
+    } catch (error: any) {
+      console.log('error:', error);
+      setErrors(error?.response?.data || {});
+    }
+  };
+
+  return (
+    <div className="bg-white">
+      <div className="flex flex-col items-center justify-center h-screen p-6">
+        <div className="w-10/12 mx-auto md:w-96">
+          <h1 className="mb-2 text-lg font-medium">회원가입</h1>
+          <form onSubmit={handleSubmit}>
+            <InputGroup
+              placeholder="Email"
+              value={email}
+              setValue={setEmail}
+              error={errors.email}
+            />
+            <InputGroup
+              placeholder="Username"
+              value={username}
+              setValue={setUsername}
+              error={errors.username}
+            />
+            <InputGroup
+              placeholder="Password"
+              value={password}
+              setValue={setPassword}
+              error={errors.password}
+            />
+            <button className="w-full py-2 mb-1 text-xs font-bold text-white uppercase bg-gray-400 border border-gray-400 rounded">
+              Sign Up
+            </button>
+          </form>
+          <small>
+            이미 가입하셨나요?
+            <Link href="/login" className="ml-1 text-blue-500 uppercase">
+              로그인
+            </Link>
+          </small>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default register;
