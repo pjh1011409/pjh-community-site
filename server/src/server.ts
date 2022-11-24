3ab8e7d9840f6f381e437e96cd839556e77f1c6f
ParkJeongHo <pjh31208@naver.com>
2022-11-24 21:23:19 +0900

    feat : Set api path and cookie parser
    클라이언트의 게시글 작성 데이터 요청에 대한 api경로 설정 및 CORS cookie설정

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import morgan from 'morgan';
 import { AppDataSource } from './data-source';
 import authRoutes from './routes/auth';
+import subRoutes from './routes/subs';
+import cookieParser from 'cookie-parser';
+
 import cors from 'cors';
 import dotenv from 'dotenv';
 
@@ -11,10 +14,12 @@ const origin = 'http://localhost:3000';
 app.use(cors({ origin, credentials: true }));
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cookieParser());
 dotenv.config();
 
 app.get('/', (_, res) => res.send('running'));
 app.use('/api/auth', authRoutes);
+app.use('/api/subs', subRoutes);
 
 const port = 4000;
 
