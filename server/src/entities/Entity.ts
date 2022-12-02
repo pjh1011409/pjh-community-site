e04c2556847c115fa3b0cd76a3369c47bd598d4a
ParkJeongHo <pjh31208@naver.com>
2022-12-02 23:22:34 +0900

    feat : Settings to get image url

--- server/src/entities/Entity.ts
+++ server/src/entities/Entity.ts
@@ -1,3 +1,4 @@
+import { instanceToPlain } from 'class-transformer';
 import {
   BaseEntity,
   CreateDateColumn,
@@ -14,4 +15,8 @@ export default abstract class Entity extends BaseEntity {
 
   @UpdateDateColumn()
   updateAt: Date;
+
+  toJSON() {
+    return instanceToPlain(this);
+  }
 }
