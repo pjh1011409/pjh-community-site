53cab29dc11a719dd74ca322e9b794d88fa415a5
ParkJeongHo <pjh31208@naver.com>
2022-12-02 23:24:11 +0900

    feat : Create an API for client request

--- server/src/routes/subs.ts
+++ server/src/routes/subs.ts
@@ -1,11 +1,17 @@
 import { Request, Response, Router } from 'express';
-
+import Post from '../entities/Post';
+import multer, { FileFilterCallback } from 'multer';
 import userMiddleware from '../middlewares/user';
 import authMiddleware from '../middlewares/auth';
 import { AppDataSource } from '../data-source';
 import Sub from '../entities/Sub';
 import { isEmpty } from 'class-validator';
 import User from '../entities/User';
+import { NextFunction } from 'express-serve-static-core';
+import { callbackify } from 'util';
+import path from 'path';
+import { unlinkSync } from 'fs';
+import { makeId } from '../utils/helpers';
 
 const createSub = async (req: Request, res: Response, next) => {
   const { name, title, description } = req.body;
@@ -45,8 +51,129 @@ const createSub = async (req: Request, res: Response, next) => {
   }
 };
 
+const topSubs = async (_: Request, res: Response) => {
+  try {
+    const imageUrlExp = `COALESCE('${process.env.APP_URL}/images/' || s."imageUrn",'https://www.gravatar.com/avatar?d=mp&f=y')`;
+    const subs = await AppDataSource.createQueryBuilder()
+      .select(
+        `s.title, s.name, ${imageUrlExp} as "imageUrl", count(p.id) as "postCount"`
+      )
+      .from(Sub, 's')
+      .leftJoin(Post, 'p', `s.name = p."subName"`)
+      .groupBy('s.title, s.name, "imageUrl"')
+      .orderBy(`"postCount"`, 'DESC')
+      .limit(5)
+      .execute();
+    return res.json(subs);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: '문제가 발생했습니다.' });
+  }
+};
+
+const getSub = async (req: Request, res: Response) => {
+  const name = req.params.name;
+
+  try {
+    const sub = await Sub.findOneByOrFail({ name });
+
+    return res.json(sub);
+  } catch (error) {
+    return res.status(404).json({ error: '게시글을 찾을 수 없습니다.' });
+  }
+};
+
+const ownSub = async (req: Request, res: Response, next: NextFunction) => {
+  const user: User = res.locals.user;
+
+  try {
+    const sub = await Sub.findOneOrFail({ where: { name: req.params.name } });
+
+    if (sub.username !== user.username) {
+      return res
+        .status(403)
+        .json({ error: '사용자님의 소유 게시글이 아닙니다.' });
+    }
+
+    res.locals.sub = sub;
+
+    return next();
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: '문제가 발생하였습니다' });
+  }
+};
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: 'public/images',
+    filename: (_, file, callback) => {
+      const name = makeId(10);
+      callback(null, name + path.extname(file.originalname));
+    },
+  }),
+  fileFilter: (_, file: any, callback: FileFilterCallback) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      callback(null, true);
+    } else {
+      callback(new Error('이미지가 아닙니다.'));
+    }
+  },
+});
+
+const uploadSubImage = async (req: Request, res: Response) => {
+  const sub: Sub = res.locals.sub;
+  try {
+    const type = req.body.type;
+
+    if (type !== 'image' && type !== 'banner') {
+      if (!req.file?.path) {
+        return res.status(400).json({ error: '유효하지 않는 파일입니다.' });
+      }
+
+      unlinkSync(req.file.path);
+      return res.status(400).json({ error: '잘못된 유형입니다.' });
+    }
+
+    let oldImageUrn = '';
+
+    if (type === 'image') {
+      oldImageUrn = sub.imageUrn || '';
+      sub.imageUrn = req.file?.filename || '';
+    } else if (type === 'banner') {
+      oldImageUrn = sub.bannerUrn || '';
+      sub.bannerUrn = req.file?.filename || '';
+    }
+    await sub.save();
+
+    if (oldImageUrn !== '') {
+      const fullFilename = path.resolve(
+        process.cwd(),
+        'public',
+        'images',
+        oldImageUrn
+      );
+      unlinkSync(fullFilename);
+    }
+    return res.json(sub);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: '문제가 발생하였습니다' });
+  }
+};
+
 const router = Router();
 
+router.get('/:name', userMiddleware, getSub);
 router.post('/', userMiddleware, authMiddleware, createSub);
+router.post('/sub/topSubs', topSubs);
+router.post(
+  '/:name/upload',
+  userMiddleware,
+  authMiddleware,
+  ownSub,
+  upload.single('file'),
+  uploadSubImage
+);
 
 export default router;
