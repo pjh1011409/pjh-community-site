a4cee291aab7532c889519a0ca400893bef5905a
ParkJeongHo <pjh31208@naver.com>
2022-12-02 23:18:13 +0900

    feat : Create Main page
    네비게이션바와 사이드바와 커뮤니티 리스트가 출력되는 메인페이지 생성

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,74 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
+import type { NextPage } from 'next';
+import axios from 'axios';
+import { Sub } from '../types';
+import useSWR from 'swr';
+import { useAuthState } from '../context/auth';
 
-export default function Home() {
-  return <div className="text-red-900 text-6xl font-black">안녕</div>;
-}
+const Home: NextPage = () => {
+  const { authenticated } = useAuthState();
+  const fetcher = async (url: string) => {
+    return await axios.get(url).then(res => res.data);
+  };
+  const address = 'http://localhost:4000/api/subs/sub/topSubs';
+  const { data: topSubs } = useSWR<Sub[]>(address, fetcher);
+
+  return (
+    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+      {/* 포스트 리스트 */}
+      <div className="w-full md:mr-3 md:w-8/12"></div>
+
+      {/* 사이드바 */}
+      <div className="hidden w-4/12 ml-3 md:block">
+        <div className="bg-white border rounded">
+          <div className="p-4 border-b">
+            <p className="text-lg font-semibold text-center">상위 커뮤니티</p>
+          </div>
+
+          {/* 커뮤니티 리스트 */}
+          <div>
+            {topSubs?.map(sub => (
+              <div
+                key={sub.name}
+                className="flex items-center px-4 py-2 text-xs border-b"
+              >
+                <Link href={`/r/${sub.name}`}>
+                  <a>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </a>
+                </Link>
+                <Link href={`/r/${sub.name}`}>
+                  <a className="ml-2 font-bold hover:cursor-pointer">
+                    /r/{sub.name}
+                  </a>
+                </Link>
+                <p className="ml-auto font-md">{sub.postCount}</p>
+              </div>
+            ))}
+          </div>
+          {authenticated && (
+            <div className="w-full py-6 text-center">
+              <Link
+                href="/subs/create"
+                className="w-full p-2 text-center text-white bg-gray-400 rounded"
+              >
+                커뮤니티 만들기
+              </Link>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
