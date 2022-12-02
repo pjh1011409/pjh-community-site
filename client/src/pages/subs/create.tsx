8563f586fa6ae76a8153be0c854ff6bc53219647
ParkJeongHo <pjh31208@naver.com>
2022-12-02 23:19:45 +0900

    fix : Resolving authentication processing errors
    잘못된 api경로설정으로 인한 인증처리 오류 해결

--- client/src/pages/subs/create.tsx
+++ client/src/pages/subs/create.tsx
@@ -84,7 +84,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
     const cookie = req.headers.cookie;
     if (!cookie) throw new Error('Missing auth token cookie');
 
-    await axios.get('/auth/me', {
+    await axios.get(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/api/auth/me`, {
       headers: { cookie },
     });
     return { props: {} };
