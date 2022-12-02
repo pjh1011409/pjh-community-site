e04ce693bd9dac746caa2af384dd477cc67dec5b
ParkJeongHo <pjh31208@naver.com>
2022-12-02 23:22:57 +0900

    feat : Add type setting

--- client/src/types.tsx
+++ client/src/types.tsx
@@ -4,3 +4,54 @@ export interface User {
   createdAt: string;
   updatedAt: string;
 }
+
+export interface Sub {
+  createdAt: string;
+  updatedAt: string;
+  name: string;
+  title: string;
+  description: string;
+  imageUrn: string;
+  bannerUrn: string;
+  username: string;
+  posts: Post[];
+  postCount?: string;
+
+  imageUrl: string;
+  bannerUrl: string;
+}
+export interface Post {
+  identifier: string;
+  title: string;
+  slug: string;
+  body: string;
+  subName: string;
+  username: string;
+  createdAt: string;
+  updatedAt: string;
+  sub?: Sub;
+
+  url: string;
+  userVote?: number;
+  votesScore?: number;
+  commentCount?: number;
+}
+
+export interface User {
+  username: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+}
+
+export interface Comment {
+  identifier: string;
+  body: string;
+  username: string;
+  createdAt: string;
+  updatedAt: string;
+  post?: Post;
+
+  userVote: number;
+  voteScore: number;
+}
