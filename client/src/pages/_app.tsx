d0a5397e97daf21586e16ed52701748dba0d0f4c
ParkJeongHo <pjh31208@naver.com>
2022-12-04 00:19:01 +0900

    feat : Global fetcher settings and icon usage

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -4,6 +4,9 @@ import Axios from 'axios';
 import { AuthProvider } from '../context/auth';
 import { useRouter } from 'next/router';
 import NavBar from '../components/navBar';
+import axios from 'axios';
+import { SWRConfig } from 'swr';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   Axios.defaults.baseURL = process.env.NEXT_PUBLIC_SERVER_BASE_URL + '/api';
@@ -13,12 +16,33 @@ export default function App({ Component, pageProps }: AppProps) {
   const authRoutes = ['/register', '/login'];
   const authRoute = authRoutes.includes(pathname);
 
+  const fetcher = async (url: string) => {
+    try {
+      const res = await axios.get(url);
+      return res.data;
+    } catch (error: any) {
+      throw error.response.data;
+    }
+  };
+
   return (
-    <AuthProvider>
-      {!authRoute && <NavBar />}
-      <div className={authRoute ? '' : 'pt-20'}>
-        <Component {...pageProps} />
-      </div>
-    </AuthProvider>
+    <>
+      <>
+        <script
+          defer
+          src="https://use.fontawesome.com/releases/v6.1.1/js/all.js"
+          integrity="sha384-xBXmu0dk1bEoiwd71wOonQLyH+VpgR1XcDH3rtxrLww5ajNTuMvBdL5SOiFZnNdp"
+          crossOrigin="anonymous"
+        ></script>
+      </>
+      <SWRConfig value={{ fetcher }}>
+        <AuthProvider>
+          {!authRoute && <NavBar />}
+          <div className={authRoute ? '' : 'pt-20'}>
+            <Component {...pageProps} />
+          </div>
+        </AuthProvider>
+      </SWRConfig>
+    </>
   );
 }
