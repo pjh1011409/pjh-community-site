0f468a3e251e99bae45b37d337ebe5dfe1674fc2
ParkJeongHo <pjh31208@naver.com>
2022-12-04 00:31:17 +0900

    feat : Set router route

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,9 @@ import morgan from 'morgan';
 import { AppDataSource } from './data-source';
 import authRoutes from './routes/auth';
 import subRoutes from './routes/subs';
+import postRoutes from './routes/posts';
+import voteRoutes from './routes/votes';
+
 import cookieParser from 'cookie-parser';
 
 import cors from 'cors';
@@ -20,6 +23,8 @@ dotenv.config();
 app.get('/', (_, res) => res.send('running'));
 app.use('/api/auth', authRoutes);
 app.use('/api/subs', subRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/votes', voteRoutes);
 
 app.use(express.static('public'));
 
