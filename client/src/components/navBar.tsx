a9c182484abea619ad06a8c17b99a5382de05c1a
ParkJeongHo <pjh31208@naver.com>
2022-12-04 18:35:23 +0900

    chore : Navbar image upload test

--- client/src/components/navBar.tsx
+++ client/src/components/navBar.tsx
@@ -22,14 +22,14 @@ export const NavBar: React.FC = () => {
   return (
     <div className="fixed inset-x-0 top-0 z-10 flex items-center justify-between px-5 bg-white h-13">
       <span className="text-2xl font-semibold text-gray-400">
-        <Link href="/">Community</Link>
+        <Link href="/">community</Link>
       </span>
       <div className="max-w-full px-4">
         <div className="relative flex items-center bg-gray-100 border rounded hover:border-gray-700 hover:bg-white">
           <FaSearch className="ml-2 text-gray-400" />
           <input
             type="text"
-            placeholder="Search Reddit"
+            placeholder="Search..."
             className="px-3 py-1 bg-transparent rounded h-7 focus:outline-none"
           />
         </div>
