7c258931dc78ec29fe2128eadaf339518417096b
ParkJeongHo <pjh31208@naver.com>
2022-12-09 17:02:14 +0900

    feat : Create Search UI

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -8,6 +8,7 @@ import { useAuthState } from '../context/auth';
 import useSWRInfinite from 'swr/infinite';
 import PostCard from '../components/postCard';
 import { useEffect, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 
 const Home: NextPage = () => {
   const { authenticated } = useAuthState();
@@ -66,64 +67,77 @@ const Home: NextPage = () => {
   };
 
   return (
-    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
-      {/* 포스트 리스트 */}
-      <div className="w-full md:mr-3 md:w-8/12">
-        {isInitialLoading && <p className="text-lg text-center">Loading...</p>}
-        {posts?.map(post => (
-          <PostCard post={post} key={post.identifier} mutate={mutate} />
-        ))}
-        {isValidating && posts.length > 0 && (
-          <p className="text-lg text-center">Loading More...</p>
-        )}
+    <>
+      <div className="relative flex items-center bg-gray-100 border rounded hover:border-gray-700 hover:bg-white">
+        <FaSearch className="ml-2 text-gray-400" />
+        <input
+          type="search"
+          placeholder="Search..."
+          className="px-3 py-1 bg-transparent rounded h-7 focus:outline-none"
+        />
       </div>
-
-      {/* 사이드바 */}
-      <div className="hidden w-4/12 ml-3 md:block">
-        <div className="bg-white border rounded">
-          <div className="p-4 border-b">
-            <p className="text-lg font-semibold text-center">상위 커뮤니티</p>
-          </div>
-
-          {/* 커뮤니티 리스트 */}
-          <div>
-            {topSubs?.map(sub => (
-              <div
-                key={sub.name}
-                className="flex items-center px-4 py-2 text-xs border-b"
-              >
-                <Link href={`/r/${sub.name}`}>
-                  <Image
-                    src={sub.imageUrl}
-                    className="rounded-full cursor-pointer"
-                    alt="Sub"
-                    width={24}
-                    height={24}
-                  />
-                </Link>
-                <Link
-                  href={`/r/${sub.name}`}
-                  className="ml-2 font-bold hover:cursor-pointer"
-                >
-                  /r/{sub.name}
-                </Link>
-                <p className="ml-auto font-md">{sub.postCount}</p>
-              </div>
-            ))}
-          </div>
-          {authenticated && (
-            <div className="w-full py-6 text-center">
-              <Link
-                href="/subs/create"
-                className="w-full p-2 text-center text-white bg-gray-400 rounded"
-              >
-                커뮤니티 만들기
-              </Link>
-            </div>
+      <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+        <div className="max-w-full px-4"></div>
+        {/* 포스트 리스트 */}
+        <div className="w-full md:mr-3 md:w-8/12">
+          {isInitialLoading && (
+            <p className="text-lg text-center">Loading...</p>
+          )}
+          {posts.map(post => (
+            <PostCard post={post} key={post.identifier} mutate={mutate} />
+          ))}
+          {isValidating && posts.length > 0 && (
+            <p className="text-lg text-center">Loading More...</p>
           )}
         </div>
+
+        {/* 사이드바 */}
+        <div className="hidden w-4/12 ml-3 md:block">
+          <div className="bg-white border rounded">
+            <div className="p-4 border-b">
+              <p className="text-lg font-semibold text-center">상위 커뮤니티</p>
+            </div>
+
+            {/* 커뮤니티 리스트 */}
+            <div>
+              {topSubs?.map(sub => (
+                <div
+                  key={sub.name}
+                  className="flex items-center px-4 py-2 text-xs border-b"
+                >
+                  <Link href={`/r/${sub.name}`}>
+                    <Image
+                      src={sub.imageUrl}
+                      className="rounded-full cursor-pointer"
+                      alt="Sub"
+                      width={24}
+                      height={24}
+                    />
+                  </Link>
+                  <Link
+                    href={`/r/${sub.name}`}
+                    className="ml-2 font-bold hover:cursor-pointer"
+                  >
+                    /r/{sub.name}
+                  </Link>
+                  <p className="ml-auto font-md">{sub.postCount}</p>
+                </div>
+              ))}
+            </div>
+            {authenticated && (
+              <div className="w-full py-6 text-center">
+                <Link
+                  href="/subs/create"
+                  className="w-full p-2 text-center text-white bg-gray-400 rounded"
+                >
+                  커뮤니티 만들기
+                </Link>
+              </div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
