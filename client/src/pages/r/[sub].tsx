d5c0990eb72db210126134c39a517fa995bc592d
ParkJeongHo <pjh31208@naver.com>
2022-12-12 01:04:12 +0900

    refactor : Postpage & CommunityPage component division

--- client/src/pages/r/[sub].tsx
+++ client/src/pages/r/[sub].tsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
-import { useState, useEffect } from 'react';
-import Image from 'next/image';
-import Axios from 'axios';
 import { useRouter } from 'next/router';
-import SideBar from '../../components/SideBar';
+import SideBar from '../../components/subPage/SideBar';
 import useSWR from 'swr';
-import { useAuthState } from '../../context/auth';
-import PostCard from '../../components/PostCard';
+import PostCard from '../../components/common/PostCard';
 import { Post } from '../../types';
+import SubHeader from '../../components/subPage/SubHeader';
 
 const SubPage = () => {
-  const [ownSub, setOwnSub] = useState(false);
-  const { authenticated, user } = useAuthState();
-  const fileInputRef = React.useRef<HTMLInputElement>(null);
-
   const router = useRouter();
   const subName = router.query.sub;
   const {
@@ -21,37 +14,6 @@ const SubPage = () => {
     error,
     mutate,
   } = useSWR(subName ? `/subs/${subName}` : null);
-
-  useEffect(() => {
-    if (!sub || !user) return;
-    setOwnSub(authenticated && user.username === sub.username);
-  }, [sub]);
-  const openFileInput = (type: string) => {
-    if (!ownSub) return;
-    const fileInput = fileInputRef.current;
-    if (fileInput) {
-      fileInput.name = type;
-      fileInput.click();
-    }
-  };
-
-  const uploadImage = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files === null) return;
-    const file = e.target.files[0];
-
-    const formData = new FormData();
-    formData.append('file', file);
-    formData.append('type', fileInputRef.current!.name);
-
-    try {
-      await Axios.post(`/subs/${sub.name}/upload`, formData, {
-        headers: { 'context-Type': 'multipart/form-data' },
-      });
-      mutate();
-    } catch (error: any) {
-      console.log(error);
-    }
-  };
 
   let renderPosts;
   if (!sub) {
@@ -70,62 +32,10 @@ const SubPage = () => {
     <>
       {sub && (
         <>
-          <input
-            type="file"
-            hidden={true}
-            ref={fileInputRef}
-            onChange={uploadImage}
-          />
-          <div>
-            {/* 배너 이미지 */}
-            <div className="bg-gray-400">
-              {sub.bannerUrl ? (
-                <div
-                  className="h-56"
-                  style={{
-                    backgroundImage: `url(${sub.bannerUrl})`,
-                    backgroundRepeat: 'no-repeat',
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center',
-                  }}
-                  onClick={() => openFileInput('banner')}
-                ></div>
-              ) : (
-                <div
-                  className="h-20 bg-gray-400"
-                  onClick={() => openFileInput('banner')}
-                ></div>
-              )}
-            </div>
-            {/* 커뮤니티 메타 데이터 */}
-            <div className="h-20 bg-white">
-              <div className="relative flex max-w-5xl px-5 mx-auto">
-                <div className="absolute" style={{ top: -15 }}>
-                  {sub.imageUrl && (
-                    <Image
-                      src={sub.imageUrl}
-                      alt="커뮤니티 이미지"
-                      width={70}
-                      height={70}
-                      className="rounded-full"
-                      onClick={() => openFileInput('image')}
-                    />
-                  )}
-                </div>
-                <div className="pt-1 pl-24">
-                  <div className="flex items-center">
-                    <h1 className="text-3xl font-bold ">{sub.title}</h1>
-                  </div>
-                  <p className="font-bold text-gray-400 text-small">
-                    {sub.name}
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+          <SubHeader />
           {/* 포스트와 사이드바 */}
-          <div className="flex max-w-5xl px-4 pt-5 mx-auto">
-            <div className="w-full md:mr-3 md:w-8/12">{renderPosts}</div>
+          <div className="flex flex-wrap-reverse max-w-6xl px-4 pt-5 justify-center mx-auto">
+            <div className="w-full md:mr-3 md:w-6/12">{renderPosts}</div>
             <SideBar sub={sub} mutate={mutate} />
           </div>
         </>
