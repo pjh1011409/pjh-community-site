a64dec7e2ed116422c2e4d25638f771b215434ce
ParkJeongHo <pjh31208@naver.com>
2022-12-13 17:28:15 +0900

    design : User Page CSS design

--- client/src/pages/u/[username].tsx
+++ client/src/pages/u/[username].tsx
@@ -1,26 +1,24 @@
-import dayjs from 'dayjs';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import React from 'react';
 import useSWR from 'swr';
 import PostCard from '../../components/common/PostCard';
+import CommentList from '../../components/userPage/CommentList';
+import UserInfo from '../../components/userPage/UserInfo';
 import { Comment, Post } from '../../types';
 
 const UserPage = () => {
   const router = useRouter();
   const username = router.query.username;
 
-  const { data, error, mutate } = useSWR(
-    username ? `/users/${username}` : null
-  );
+  const { data, mutate } = useSWR(username ? `/users/${username}` : null);
 
   if (!data) return null;
 
   return (
-    <div className="flex max-w-5xl px-4 pt-5 mx-auto">
+    <div className="flex flex-wrap-reverse mx-auto mt-10 items-end">
       {/* 유저 포스트 댓글 리스트 */}
-      <div className="w-full md:mr-3 md:w-8/12">
+
+      <div className="w-11/12  md:w-8/12 lg:w-5/12 mx-auto my-4 h-auto">
         {data.userData.map((data: any) => {
           if (data.type === 'Post') {
             const post: Post = data;
@@ -29,64 +27,11 @@ const UserPage = () => {
             );
           } else {
             const comment: Comment = data;
-            return (
-              <div
-                key={comment.identifier}
-                className="flex my-4 bg-white rounded"
-              >
-                <div className="flex-shrink-0 w-10 py-10 text-center bg-white border-r rounded-l">
-                  <i className="text-gray-500 fas fa-comment-alt fa-xs"></i>
-                </div>
-                <div className="w-full p-2">
-                  <p className="mb-2 text-xs text-gray-500">
-                    <Link
-                      href={`/u/${comment.username}`}
-                      className="cursor-pointer hover:underline"
-                    >
-                      {comment.username}님의
-                    </Link>
-                    <span> commented on</span>
-                    <Link
-                      href={`/u/${comment.post?.url}`}
-                      className="font-semibold cursor-pointer hover:underline"
-                    >
-                      {comment.post?.title}
-                    </Link>
-                    <span>•</span>
-                    <Link
-                      href={`/u/${comment.post?.subName}`}
-                      className="text-black cursor-pointer hover:underline"
-                    >
-                      /r/{comment.post?.subName}
-                    </Link>
-                  </p>
-                  <hr />
-                  <p className="p-1">{comment.body}</p>
-                </div>
-              </div>
-            );
+            return <CommentList key={comment.identifier} comment={comment} />;
           }
         })}
       </div>
-      {/* 유저 정보 */}
-      <div className="hidden w-4/12 ml-3 md:block">
-        <div className="flex items-center p-3 bg-gray-400 rounded-t">
-          <Image
-            src="https://www.gravatar.com/avatar/0000?d=mp&f=y"
-            alt="user profile"
-            className="mx-auto border border-white rounded-full"
-            width={40}
-            height={40}
-          />
-          <p className="pl-2 text-md">{data.user.username}</p>
-        </div>
-        <div className="p-2 bg-white rounded-b">
-          <p>
-            {dayjs(data.user.createdAt).format('YYYY.MM.DD')}
-            가입
-          </p>
-        </div>
-      </div>
+      <UserInfo user={data.user} />
     </div>
   );
 };
